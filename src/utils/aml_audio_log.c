15e58c626adce94e6f7d7b7b58b510f3a7ea08ad
yujie.wu <yujie.wu@amlogic.com>
2018-12-05 11:12:22 +0800

    audio: Add dynamic log level control [1/1]
    PD#SWPL-2282
    
    Problem:
      We don't have dynamic log level control
    
    Solution:
      Add dymanic debug level control
    
    Verify:
      A113
    
    Change-Id: I9d9e41712013c47af74057b23ec9c872c000da06
    Signed-off-by: yujie.wu <yujie.wu@amlogic.com>

--- src/utils/aml_audio_log.c
+++ src/utils/aml_audio_log.c
@@ -19,11 +19,19 @@
 #include <sys/prctl.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/inotify.h>
+
 #include "log.h"
+
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define BUF_LEN     ( 16 * ( EVENT_SIZE + 16 ) )
+
 
 typedef struct aml_audio_log {
     int bExit;
     pthread_t threadid;
+    int notify_fd;
+    int watch_desc;
 
 
 } aml_audio_log_t;
@@ -36,28 +44,58 @@ void *aml_log_threadloop(void *data)
     aml_audio_log_t * log_handle = (aml_audio_log_t *)data;
     char *value;
     char *end;
+    char *token = NULL;
     int debug_level = LEVEL_ERROR;
+    char buffer[BUF_LEN];
+    int len = 0;
+    FILE *file_fd = NULL;
+    char * debug_file = "/tmp/AML_AUDIO_DEBUG";
 
     prctl(PR_SET_NAME, (unsigned long)"aml_audio_log");
 
     while (!log_handle->bExit) {
+        log_handle->watch_desc = inotify_add_watch(log_handle->notify_fd , debug_file , IN_MODIFY | IN_CREATE);
+        len = read(log_handle->notify_fd, buffer, BUF_LEN);
 
-        value = getenv("AML_AUDIO_DEBUG");
-        if (value) {
-
-            debug_level = (int)strtol(value, &end, 0);
-            if (debug_level != aml_audio_debug_level) {
-                printf("Debug level changed from =%d to %d\n", aml_audio_debug_level, debug_level);
-                aml_audio_debug_level = debug_level;
-            }
-
-
+        if (len < 0) {
+            printf("read error\n");
+            usleep(10 * 1000);
+            continue;
         }
 
-        usleep(500 * 1000);
+        file_fd = fopen(debug_file, "r");
+
+        if (file_fd == NULL) {
+            usleep(50 * 1000);
+            continue;
+        }
+
+        len = fread(buffer, 1, 128, file_fd);
+
+        if (len <= 0) {
+            usleep(50 * 1000);
+            continue;
+        }
+
+        if (strncmp("AML_AUDIO_DEBUG", buffer, 15)) {
+            printf("wrong debug string\n");
+            usleep(50 * 1000);
+            continue;
+        }
+        token = strtok(buffer, "=");
+        if (token != NULL) {
+            token = strtok(NULL, "=");
+            if (token != NULL) {
+                debug_level = (int)strtol(token, &end, 0);
+                if (debug_level != aml_audio_debug_level) {
+                    printf("Debug level changed from =%d to %d\n", aml_audio_debug_level, debug_level);
+                    aml_audio_debug_level = debug_level;
+                }
+            }
+        }
+
+        usleep(100 * 1000);
     }
-
-
     return ((void *) 0);
 }
 
@@ -73,6 +111,7 @@ void aml_log_init(void)
 
 
     aml_log_handle->bExit = 0;
+    aml_log_handle->notify_fd = inotify_init();
     pthread_create(&(aml_log_handle->threadid), NULL,
                    &aml_log_threadloop, aml_log_handle);
 
@@ -82,6 +121,8 @@ void aml_log_init(void)
 void aml_log_exit(void)
 {
     aml_log_handle->bExit = 1;
+    inotify_rm_watch( aml_log_handle->notify_fd, aml_log_handle->watch_desc);
+    close(aml_log_handle->notify_fd);
     pthread_join(aml_log_handle->threadid, NULL);
     printf("Exit %s\n", __func__);
     return;
